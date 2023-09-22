4f297246abb5d491694d1dc9d5ecef58c1f8940c
DlbSt <dalibor.stys@synchronics.at>
2023-09-22 15:28:29 +0200

    [add] Initial commit

---
+++ WPFMidiApp/Behaviors/TextBoxCaretIndexBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+
+namespace WPFMidiApp.Behaviors
+{
+    public class TextBoxCaretIndexBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.TextChanged += TextBox_TextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.TextChanged -= TextBox_TextChanged;
+
+            base.OnDetaching();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AssociatedObject.CaretIndex = AssociatedObject.Text.Length;
+            AssociatedObject.ScrollToEnd();
+        }
+    }
+}
